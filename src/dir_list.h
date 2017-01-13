81c4d8fe5246d0b8684306233b1021659a39f87b
Zetsubold <oriondouglas@gmail.com>
2017-01-13 19:37:14 +0930

    created version 1.0
    basic version - nothing fancy

---
+++ src/dir_list.h
@@ -0,0 +1,17 @@
+#ifndef DIR_LIST_H
+#define DIR_LIST_H
+
+class FileListing {
+	// For storing the details about a particular file
+public:
+    string name;
+    time_t date;
+    FileListing(string & s, time_t t);
+};
+
+ostream& operator<< (ostream & out, const FileListing & in);
+
+void get_dir_files(vector<FileListing> & file_list, string & path_name, ostream & out = cout, bool recursion = false);
+
+
+#endif
